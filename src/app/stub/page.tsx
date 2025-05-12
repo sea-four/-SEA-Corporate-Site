bcf53bfdb86f690ace025fd9ca20f09f22a80008
shoma-s-131 <shoma.s.works131@gmail.com>
2025-05-13 05:33:37 +0900

    stubページの追加

---
+++ src/app/stub/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/Hero";
+
+export default function Stub() {
+  return (
+    <>
+      <main>
+        <Hero companyName={"株式会社SEA"} backgroundImageUrl={"/next.svg"} />
+        <Hero companyName={"株式会社SEA"} backgroundImageUrl={"/next.svg"} />
+      </main>
+    </>
+  );
+}
